f7afad9fe7a25d7c3920187ef927978ae3b83f00
Kang-min Liu <gugod@gugod.org>
2018-12-05 09:12:51 +0900

    minor refactoring

--- test.sh
+++ test.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
+
+assert_rc_ok() {
+    if [[ $rc -ne 0 ]]; then
+        echo FAIL. Exit Status: $rc
+        echo
+        exit $rc
+    fi    
+}
+
 cd
 curl -sS -L https://install.perlbrew.pl | bash
 source ~/perl5/perlbrew/etc/bashrc
 
 perlbrew info
 rc=$?
-if [[ $rc -ne 0 ]]; then
-    echo FAIL. Exit Status: $rc
-    echo
-    exit $rc
-fi
+assert_rc_ok
 
 perlbrew available
 rc=$?
-if [[ $rc -ne 0 ]]; then
-    echo FAIL. Exit Status: $rc
-    echo
-    exit $rc
-fi
+assert_rc_ok
 
 echo
 echo '__END__'
