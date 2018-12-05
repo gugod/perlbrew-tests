05dfad5cd3299c9614097dbfbcdf4c9ac4b84019
Kang-min Liu <gugod@gugod.org>
2018-12-05 09:06:40 +0900

    silent but error-showing.

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd
-curl -L https://install.perlbrew.pl | bash
+curl -sS -L https://install.perlbrew.pl | bash
 source ~/perl5/perlbrew/etc/bashrc
 
 perlbrew info
 rc=$?
 
-echo '========'
+echo
+echo '__END__'
 echo DONE
 echo 
 exit $rc
