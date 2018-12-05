6a358c2e3985d15dfb3a0a924155a687e53b8abd
Kang-min Liu <gugod@gugod.org>
2018-12-05 09:10:39 +0900

    try running `available` command

--- test.sh
+++ test.sh
@@ -5,6 +5,19 @@ source ~/perl5/perlbrew/etc/bashrc
 
 perlbrew info
 rc=$?
+if [[ $rc -ne 0 ]]; then
+    echo FAIL. Exit Status: $rc
+    echo
+    exit $rc
+fi
+
+perlbrew available
+rc=$?
+if [[ $rc -ne 0 ]]; then
+    echo FAIL. Exit Status: $rc
+    echo
+    exit $rc
+fi
 
 echo
 echo '__END__'
